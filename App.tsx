d91ef4e546b5161c12cafa2f71323aa7e636744e
Sean Jiang <seanjiangsh@gmail.com>
2026-04-06 09:47:12 +0800

    feat: Exercise App Layout

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Our React Native App</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <View style={styles.searchHeaderContainer}>
+          <Text style={styles.searchText}>Search</Text>
+        </View>
+        <View style={styles.mainContent}>
+          <ul>
+            <li>Coffee</li>
+            <li>Tea</li>
+            <li>Milk</li>
+          </ul>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -16,5 +28,22 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  searchHeaderContainer: {
+    width: "100%",
+    paddingHorizontal: 20,
+    backgroundColor: "#8f8f8f",
+  },
+  searchText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  mainContent: {
+    flex: 1,
+    width: "100%",
+    paddingHorizontal: 20,
+    backgroundColor: "#e0e0e0",
   },
 });
