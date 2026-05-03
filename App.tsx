28df1224a25c1651d1b5e69f3c8d307fb02ad01d
Sean Jiang <seanjiangsh@gmail.com>
2026-05-03 09:58:25 +0800

    feat: Restaurant Info Setup-1

--- App.tsx
+++ App.tsx
@@ -1,35 +1,11 @@
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import DrinksSearchList from "./src/components/DrinksSearchList";
 
-// console.log("Platform:", Platform.OS, StatusBar.currentHeight);
-
-const DRINKS = ["Coffee", "Tea", "Milk"];
+import RestaurantsScreen from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
     <PaperProvider>
-      <SafeAreaProvider>
-        <SafeAreaView style={styles.safeArea}>
-          <View style={styles.container}>
-            <DrinksSearchList drinks={DRINKS} />
-            <ExpoStatusBar style="auto" />
-          </View>
-        </SafeAreaView>
-      </SafeAreaProvider>
+      <RestaurantsScreen />
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  }
-});
---
+++ src/features/restaurants/screens/restaurants.screen.tsx
@@ -0,0 +1,36 @@
+import { memo } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+function RestaurantsScreen() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.searchBar}>
+          <Searchbar placeholder="Search restaurants" value="" />
+        </View>
+        <View style={styles.list}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+export default memo(RestaurantsScreen);
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  searchBar: {
+    padding: 16
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 16
+  }
+});
