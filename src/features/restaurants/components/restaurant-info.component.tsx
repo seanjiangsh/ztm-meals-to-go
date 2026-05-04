e5da5827d659838d267a76e4b99ccd473ef8657c
Sean Jiang <seanjiangsh@gmail.com>
2026-05-05 07:38:56 +0800

    feat: Restaurant Info Setup-3

--- src/features/restaurants/components/restaurant-info.component.tsx
+++ src/features/restaurants/components/restaurant-info.component.tsx
@@ -1,7 +1,29 @@
 import { memo } from "react";
 import { StyleSheet, View, Text } from "react-native";
 
-function RestaurantInfo() {
+interface RestaurantInfoProps {
+  name: string;
+  icon: string;
+  photos: Array<string>;
+  address: string;
+  isOpenNow: boolean;
+  rating: number;
+  isClosedTemporarily: boolean;
+}
+
+function RestaurantInfo(props: RestaurantInfoProps) {
+  const {
+    name = "Unknown Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg"
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily = false
+  } = props;
+
   return (
     <View>
       <Text>Restaurant Info</Text>
