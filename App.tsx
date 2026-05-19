df22d1eed0434fbad6e97ee2df772787abc3bb18
Sean Jiang <seanjiangsh@gmail.com>
2026-05-20 06:57:20 +0800

    feat:  Setup Custom Fonts

--- App.tsx
+++ App.tsx
@@ -1,3 +1,6 @@
+import { Lato_400Regular } from "@expo-google-fonts/lato";
+import { Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { useFonts } from "expo-font";
 import { Provider as PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ThemeProvider } from "styled-components";
@@ -6,6 +9,15 @@ import RestaurantsScreen from "@/features/restaurants/screens/restaurants.screen
 import { theme } from "@/infra/index";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Lato_400Regular,
+    Oswald_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
